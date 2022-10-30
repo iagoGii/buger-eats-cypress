75f164aef313292336025400eed04c69fdb61650
Iago Oliveira Guedes <iago.guedes@grancursosonline.com.br>
2022-10-30 20:33:24 -0300

    Adicionando informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require("cypress");
 
+module.exports = {
+  projectId: "8xwfdw"
+  // The rest of the Cypress config options go here...
+}
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
