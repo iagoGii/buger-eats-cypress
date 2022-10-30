560260c5c330615c6ca379fa550ca376f984c3cf
Iago Oliveira Guedes <iago.guedes@grancursosonline.com.br>
2022-10-30 20:28:12 -0300

    Adicionando informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      projectId: "8xwfdw"
     },
+    projectId: "8xwfdw"
   },
 });
